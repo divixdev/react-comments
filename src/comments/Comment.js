8718ec3018726470b606a6a90379867c782804cd
divixdev <ismail.otalb@ecomexperts.io>
2023-01-18 17:42:04 +0000

    display parent and child comments from json obj

--- src/comments/Comment.js
+++ src/comments/Comment.js
@@ -1,6 +1,30 @@
-const Comment= ()=>{
+const Comment= ({comment,commentsReplies,})=>{
     return (
-        <h3> Comment </h3>
+        <article className="single-comment parent"  >
+           <div className="comment-heading">
+                <img src="/user-icon.png" alt="user" />
+                <span> {comment.username}  </span>
+                <time> { new Date(comment.createdAt).toLocaleDateString() } </time>
+
+           </div>
+                <div className="comment-body">
+                    <p>{comment.body}</p>
+                     {
+                        commentsReplies.map(reply=>(
+                    <article  className="single-comment child"  >
+                    <div className="comment-heading">
+                       <img src="/user-icon.png" alt="user" />
+                       <span> {reply.username}  </span>
+                       <time> { new Date(reply.createdAt).toLocaleDateString() } </time>
+                    </div>
+                    <div className="comment-body">
+                        {reply.body}
+                    </div>
+                    </article>
+                        ))
+                     }
+                </div>
+        </article>
     )
 }
 
--- src/comments/Comments.js
+++ src/comments/Comments.js
@@ -1,6 +1,32 @@
+import { useEffect, useState } from "react"
+import { getComments } from "../api"
+import Comment from "./Comment"
+
 const Comments= ()=>{
+    let [cmtsFromBackend,setCmtsFromBackend]= useState([]);
+    useEffect(()=>{
+        getComments().then(data=> setCmtsFromBackend(data) )
+    },[])
+    const parentComments = cmtsFromBackend.filter(cmtFromBackend =>
+        cmtFromBackend.parentId === null
+     );
+     const commentsReplies = (parentComentId)=>{
+         return  cmtsFromBackend.filter(cmtFromBackend=> cmtFromBackend.parentId === parentComentId   )
+     }
+
+
     return (
-        <h3> Comments </h3>
+        <section className="comments-section">
+           <h2> Leave your comment</h2>
+         { parentComments.map(parentComment=> 
+         <Comment 
+         comment={parentComment} 
+         key={parentComment.id }
+         commentsReplies={commentsReplies(parentComment.id)}
+          /> ) }
+        </section>
+        
+        
     )
 }
 
